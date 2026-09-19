8f2ba2730d49103d63c89e16122bf676412936a4
Plotka <tahamustafaalkan@gmail.com>
2026-09-19 20:32:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-const gradients = [
-  "linear-gradient(135deg, #4a90e2, #50e3c2)", // mavi-turkuaz
-  "linear-gradient(135deg, #9b59b6, #8e33ad)", // mor tonu
-  "linear-gradient(135deg, #2ecc71, #27ae60)", // yeşil tonları
-  "linear-gradient(135deg, #f39c12, #f1c40f)", // turuncu-sarı tonları
-  "linear-gradient(135deg, #e74c3c, #c0392b)", // kırmızı tonları
-  "linear-gradient(135deg, #000000, #51535a)"  // siyah-gri tonu
-];
+const clock = document.getElementById("live-clock");
 
-let index = 0;
+function updateClock() {
+    const now = new Date();
 
-const button = document.getElementById("colorPicker");
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+    const seconds = String(now.getSeconds()).padStart(2, "0");
 
-button.addEventListener("click", () => {
-  document.body.style.background = gradients[index];
-  index = (index + 1) % gradients.length; // sırayla dön
-});
+    clock.textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+updateClock();
+
+setInterval(updateClock, 1000);
